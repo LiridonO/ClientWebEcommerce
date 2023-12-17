690b7555d9070f05232d7e9ea084f2520ce861cc
liridon osmani <liridon@albasoftgroup.com>
2023-12-17 22:24:22 +0100

    routing for home,shop,productdetails/id etc

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IBrand } from '../shared/models/brand';
 import { map } from 'rxjs/operators';
 import { IProductType } from '../shared/models/productType';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -40,6 +41,10 @@ export class ShopService {
         return response.body;
       })
     )
+  }
+
+  getProduct(id:number){
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands(){
